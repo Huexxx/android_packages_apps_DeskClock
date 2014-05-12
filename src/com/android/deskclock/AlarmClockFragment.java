1cece472fb102d769ee6879a03558060f0cf5a8e
kaiyiz <kaiyiz@codeaurora.org>
2014-05-12 15:39:00 +0800

    DeskClock: Fix alarm name display abnormal when choose audio
    Lack the handle to uri whose authority is document.
    
    When we judge the uri is from document ,we obtain its name as
    title for alarm name.
    
    CRs-fixed: 662043
    
    Change-Id: I588678ee8102adbe661a155e7650f7b50bb5b85e

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -105,6 +105,8 @@ public class AlarmClockFragment extends DeskClockFragment implements
     private static final String KEY_DELETE_CONFIRMATION = "deleteConfirmation";
     private static final String KEY_SELECT_SOURCE = "selectedSource";
 
+    private static final String DOC_AUTHORITY = "com.android.providers.media.documents";
+
     private static final int REQUEST_CODE_RINGTONE = 1;
     private static final int REQUEST_CODE_EXTERN_AUDIO = 3;
 
@@ -124,6 +126,8 @@ public class AlarmClockFragment extends DeskClockFragment implements
     private View mTimelineLayout;
     private AlarmTimelineView mTimelineView;
     private View mFooterView;
+
+    private String mDisplayName;
 
     private Bundle mRingtoneTitleCache; // Key: ringtone uri, value: ringtone title
     private ActionableToastBar mUndoBar;
@@ -1486,9 +1490,13 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 } else {
                     if (isRingToneUriValid(uri)) {
                         // This is slow because a media player is created during Ringtone object creation.
-                        Ringtone ringTone = RingtoneManager.getRingtone(mContext, uri);
-                        if (ringTone != null) {
-                            title = ringTone.getTitle(mContext);
+                        if (uri.getAuthority().equals(DOC_AUTHORITY)) {
+                            title = mDisplayName;
+                        } else {
+                            Ringtone ringTone = RingtoneManager.getRingtone(mContext, uri);
+                            if (ringTone != null) {
+                                title = ringTone.getTitle(mContext);
+                            }
                         }
                     }
                 }
@@ -1509,8 +1517,15 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 Cursor cursor = null;
                 try {
                     cursor = mContext.getContentResolver().query(uri,
-                            new String[] {MediaStore.Audio.Media.TITLE}, null, null, null);
+                            new String[] {
+                                MediaStore.Audio.Media.TITLE,
+                                MediaStore.Audio.Media.DISPLAY_NAME
+                            }, null, null, null);
                     if (cursor != null && cursor.getCount() > 0) {
+                        if (uri.getAuthority().equals(DOC_AUTHORITY)) {
+                            cursor.moveToFirst();
+                            mDisplayName = cursor.getString(1);
+                        }
                         return true;
                     }
                 } catch (Exception e) {
